921f09deba5a716c487eb660e7171f9679f3d9af
Thinksoup <105362950+Thinksoup@users.noreply.github.com>
2022-07-06 19:43:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+rightWristX = "";
+rightWristY = "";
+ScoreRightWrist = "";
 function setup() {
 	canvas = createCanvas(1240,336);
 	instializeInSetup(ping-pong);
@@ -7,10 +10,24 @@ function setup() {
     video.hide();
 }
 function draw(){
+	if(ScoreRightWrist>0.2){
+    fill("red");
+	stroke("red");
+	circle(rightWristX, rightWristY, 30);
+	}
 	image(video, 0, 0, 1240, 336);
 	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 	
 }
 function modelLoaded(){
 	console.log("modelLoaded");
+}
+function gotPoses(){
+	if(results.length>0){
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	ScoreRightWrist = results[0].pose.keypoints[10].score;
+	console.log(ScoreRightWrist);
+	}
 }
